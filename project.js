7b02f782aed694f0b0357a65f06019e452d41393
DanJan1999 <danieljanis99@gmail.com>
2021-09-13 12:50:00 -0600

    New test and final commit circleci 2.4 FINAL

--- project.js
+++ project.js
@@ -1,5 +1,10 @@
 function getGreeting(name){
     return "Hi " + name;
-}
+};
+
+function displayWinner(name, name2){
+    return name+ " beat " +name2+ "!"
+};
 
 module.exports = getGreeting;
+module.exports = displayWinner;
--- test.js
+++ test.js
@@ -1,5 +1,10 @@
 const getGreeting = require('./project');
+const displayWinner = require('./project');
 
 test('can create a basic greeting', () => {
   expect(getGreeting('Sam')).toBe("Hi Sam");
+});
+
+test('Byu won', () => {
+  expect(displayWinner('BYU', 'Utah')).toBe("BYU beat Utah!")
 });
